8d4b8d4881a0198fb2654f2b2091c2febfc9907a
williswil <32222855+williswil@users.noreply.github.com>
2023-02-10 16:16:53 -0600

    initial commit

--- Robinhood_Put_ Sales.py
+++ Robinhood_Put_ Sales.py
@@ -14,7 +14,7 @@ def open_market():
     market_open = dt.time(8,00,0)
     market_close = dt.times(15,00,0)
     if time_now > market_open and time_now < market_close:
-        maret = True
+        market = True
     else: 
         # print('### market is closed')
         pass
@@ -23,6 +23,8 @@ def open_market():
 def login_to_account(days):
     # Calculate the time range in seconds
     start = 7 * 60 * 60 + 50 * 60
+
+    
     end = 15 * 60 * 60 + 50 * 60
     wait_time = random.uniform(start, end)
 
@@ -40,22 +42,27 @@ def show_stocks():
     for stock in my_stocks:
         print(stock['symbol'])
 
-#def choose_most_profitable_put_option(stock_price, volatility, time_to_expiry, risk_free_rate, strike_prices):
+import robin_stocks as r
+
+def choose_most_profitable_put_option(stock_price, volatility, time_to_expiry, risk_free_rate, strike_prices):
+    options = r.options.find_options_by_specific_profitability("my_stocks", expirationDate=None, strikePrice=None, optionType="put", typeProfit='chance_of_profit_short', profitFloor=0.75, profitCeiling=1.0, info=None)
     max_profit = float("-inf")
-    r.options.find_options_by_specific_profitability("my_stocks",expirationDate=None,strikePrice=None,optionType="put",typeProfit='chance_of_profit_short',profitFloor=0.75,profitCeiling=1.0,info=None)
     most_profitable_strike_price = None
-    for strike_price in strike_prices:
-        put_price = sell_put_option(strike_price, stock_price, volatility, time_to_expiry, risk_free_rate)
-        profit = put_price + (stock_price - strike_price)
-        if profit > max_profit:
-            max_profit = profit
-            most_profitable_strike_price = strike_price
+    for option in options:
+        if option['chance_of_profit_short'] >= 0.75:
+            strike_price = option['strike_price']
+            put_price = sell_put_option(strike_price, stock_price, volatility, time_to_expiry, risk_free_rate)
+            profit = put_price + (stock_price - strike_price)
+            if profit > max_profit:
+                max_profit = profit
+                most_profitable_strike_price = strike_price
     if most_profitable_strike_price is not None:
         return most_profitable_strike_price
     else:
         return "No profitable options found"
 
-#def sell_put_option(strike_price, stock_price, volatility, time_to_expiry, risk_free_rate):
+
+def sell_put_option(strike_price, stock_price, volatility, time_to_expiry, risk_free_rate):
     d1 = (np.log(stock_price/strike_price) + (risk_free_rate + 0.5 * volatility**2) * time_to_expiry) / (volatility * np.sqrt(time_to_expiry))
     d2 = d1 - volatility * np.sqrt(time_to_expiry)
     put_price = strike_price * np.exp(-risk_free_rate * time_to_expiry) * stats.norm.cdf(-d2) - stock_price * stats.norm.cdf(-d1)
@@ -63,5 +70,5 @@ def show_stocks():
     if probability_of_profit >= 0.75:
         return put_price
 
-#def logout():
+def logout():
     r.authentication.logout()#
